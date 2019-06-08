00dc55332911dda269061595e545813e04af78db
SUMITHRA <51500842+Sumithra53@users.noreply.github.com>
2019-06-08 14:45:55 +0530

    Create CountDigit.java

---
+++ CountDigit.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CountDigit {
+ public static void main(String[]args){
+
+	 int count=0;
+	 Scanner scan = new Scanner(System.in);
+	 System.out.println("enter the number:");
+	 int n=scan.nextInt();
+	 while(n%10!=0) {
+	 count=count+1;
+	 n=n/10;
+	 }
+	 System.out.println(count);
+	 scan.close();
+	 }
+
+ }
+
